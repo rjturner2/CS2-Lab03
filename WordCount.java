8c2e7cc930c48370038dd0d81fe5816e552c2ee7
Ryley Turner <rjturner2@eiu.edu>
2024-01-25 13:10:36 -0600

    Solved Problem

--- WordCount.java
+++ WordCount.java
@@ -4,6 +4,32 @@ import java.io.FileNotFoundException;
 
 public class WordCount {
 	public static void main(String[] args) {
-		
+		int wordCount, lineCount, charCount;
+		wordCount = lineCount = charCount = 0;
+		try {
+			File myTextFile = new File(args[0]);
+			Scanner textFile = new Scanner(myTextFile);
+			while (textFile.hasNextLine()) {
+				String line = textFile.nextLine();
+				String[] individualWords = line.split(" ", 0);
+				wordCount += individualWords.length;
+				lineCount += 1;
+
+				for(int i = 0; i < individualWords.length; i++) {
+					for(int j = 0; j < individualWords[i].length(); j++) {
+					char c = individualWords[i].charAt(j);
+						if (!Character.isWhitespace(c)) {
+							charCount += 1;
+								}
+							}
+						}
+					}
+		System.out.print(args[0] + " has " + lineCount + " lines,");
+		System.out.print(" " + wordCount + " words, and ");
+		System.out.println(charCount + " characters.");
+				}
+	catch (FileNotFoundException e) {
+		System.out.println("That file was not found.");
+		}
 	}
 }
