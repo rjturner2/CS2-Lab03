a98dafc537848d0b5e3720643252a6ab940be5d5
Ryley Turner <rjturner2@eiu.edu>
2024-02-01 11:06:15 -0600

    Final Commit

--- WordCount.java
+++ WordCount.java
@@ -6,28 +6,27 @@ public class WordCount {
 	public static void main(String[] args) {
 		int wordCount, lineCount, charCount;
 		wordCount = lineCount = charCount = 0;
+
 		try {
 			File myTextFile = new File(args[0]);
 			Scanner textFile = new Scanner(myTextFile);
+			Scanner currentLine;
 			while (textFile.hasNextLine()) {
-				String line = textFile.nextLine();
-				String[] individualWords = line.split(" ", 0);
-				wordCount += individualWords.length;
 				lineCount += 1;
+				String line = textFile.nextLine();
+				currentLine = new Scanner(line);
 
-				for(int i = 0; i < individualWords.length; i++) {
-					for(int j = 0; j < individualWords[i].length(); j++) {
-					char c = individualWords[i].charAt(j);
-						if (!Character.isWhitespace(c)) {
-							charCount += 1;
-								}
-							}
-						}
-					}
+				while (currentLine.hasNext()) {
+					wordCount += 1;
+					String currentWord = currentLine.next();
+					charCount += currentWord.length();
+				}
+			}
 		System.out.print(args[0] + " has " + lineCount + " lines,");
 		System.out.print(" " + wordCount + " words, and ");
 		System.out.println(charCount + " characters.");
 				}
+
 	catch (FileNotFoundException e) {
 		System.out.println("That file was not found.");
 		}
